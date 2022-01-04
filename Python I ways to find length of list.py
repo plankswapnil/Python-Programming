7f51c63fd9120ee7e2e11c376b7d8851861313bb
Swapnil Paul <75802365+plankswapnil@users.noreply.github.com>
2022-01-04 16:34:47 +0530

    Python I ways to find length of list

---
+++ Python I ways to find length of list.py
@@ -0,0 +1,18 @@
+# Python code to demonstrate
+# length of list
+# using naive method
+  
+# Initializing list 
+test_list = [ 1, 4, 5, 7, 8 ]
+  
+# Printing test_list
+print ("The list is : " + str(test_list))
+  
+# Finding length of list 
+# using loop
+# Initializing counter
+counter = 0
+for i in test_list:
+      
+    # incrementing counter
+    counter = counter + 1
